e6b10ebddd29f727fed2f1e6dae8089adf25fd8f
unknown <jpgonzaleg@eafit.edu.co>
2020-10-10 09:49:38 -0500

    Código fuente y ejemplos, actualización de reflector_telescope

--- analytic/reflector_telescope.py
+++ analytic/reflector_telescope.py
@@ -9,16 +9,19 @@ from raytracing import *
 
 
 path = ImagingPath()
+path.name = "Telescopio reflector"
 
-# path.fanAngle = 0.1        # full fan angle for rays
-# path.fanNumber = 100        # number of rays in fan
-# path.rayNumber = 100        # number of points on object
+# path.objectHeight = 500
+# path.fanAngle = 0.5        # full fan angle for rays
+# path.fanNumber = 0        # number of rays in fan
+# path.rayNumber = 1000        # number of points on object
 
+showEntrancePupil = True
 path.append(Space(d=508))
 path.append(CurvedMirror(2000, diameter=127, label='Espejo Primario'))
 path.append(Space(d=450))
 path.append(Aperture(40.64, 'Espejo Secundario'))
+path.append(Space(d=63.5))
+path.append(Lens(25,13))
 
-# path.append(Space(d=25))
-# path.append(Lens(2,5))
 path.display()
